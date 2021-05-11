09d89176631754e4c461c02f3b237c492b567ed2
Kacper Wleklak <kacper.wleklak@student.put.poznan.pl>
2021-05-11 23:40:31 +0200

    Initial commit

---
+++ charades-front/src/app/pages/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  constructor() {}
+
+  ngOnInit() {
+    console.log("Hej");
+    debugger;
+  }
+  ngOnDestroy() {
+  }
+
+}
