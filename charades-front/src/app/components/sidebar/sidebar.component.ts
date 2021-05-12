340d59e0c4fd4ba2654f0c245d5b0b231f13c98d
Kacper Wleklak <kacperwleklak@gmail.com>
2021-05-12 21:17:29 +0200

    remove user profile

--- charades-front/src/app/components/sidebar/sidebar.component.ts
+++ charades-front/src/app/components/sidebar/sidebar.component.ts
@@ -10,7 +10,6 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'ni-tv-2 text-primary', class: '' },
     { path: '/icons', title: 'Icons',  icon:'ni-planet text-blue', class: '' },
-    { path: '/user-profile', title: 'User profile',  icon:'ni-single-02 text-yellow', class: '' },
     { path: '/tables', title: 'Tables',  icon:'ni-bullet-list-67 text-red', class: '' },
     { path: '/login', title: 'Login',  icon:'ni-key-25 text-info', class: '' }
 ];
